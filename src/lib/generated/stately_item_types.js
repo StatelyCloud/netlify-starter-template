4e491085e298a6c2e506aae20d4be09e62235b03
st8str <140565690+st8str@users.noreply.github.com>
2025-03-26 16:01:35 -0700

    update generated ts

--- src/lib/generated/stately_item_types.js
+++ src/lib/generated/stately_item_types.js
@@ -2,7 +2,6 @@
 /* eslint-disable */
 
 import { createClient as createGenericClient, StatelyError } from "@stately-cloud/client";
-import clientPackageJson from "@stately-cloud/client/package.json" with { "type": "json" };
 import {
   LinkSchema,
   ProfileSchema,
@@ -16,14 +15,18 @@ export const typeToSchema = {
   // objectTypes
 };
 
+
 /** The version of the schema that this client was generated for. */
 const SCHEMA_VERSION_ID = 6;
+const SCHEMA_ID = 7932271998245135;
+
 
 export function createClient(storeId, opts) {
   
-  return createGenericClient(storeId, typeToSchema, SCHEMA_VERSION_ID, opts);
+  return createGenericClient(storeId, typeToSchema, SCHEMA_VERSION_ID, SCHEMA_ID, opts);
+  
 }
 
-if (createGenericClient.length < 3) {
+if (createGenericClient.length < 4) {
   throw new Error("Your version of @stately-cloud/client is too old. Please update to the latest version.");
 }
